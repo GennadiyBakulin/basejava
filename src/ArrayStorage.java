2576d21e0bc92c0e30721408cd662c14904704cd
Геннадий Бакулин <g.bakulin@yandex.ru>
2023-01-26 20:11:11 +0700

    Реализация метода getAll()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -49,7 +51,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
